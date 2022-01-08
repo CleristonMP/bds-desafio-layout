bbdb197181b449774c36517258daa3b8d711cf60
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2022-01-08 20:33:58 -0300

    Fix Search Box layout on 576, 768

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -4,22 +4,25 @@ import './styles.css';
 const Catalog = () => {
   return (
     <div className="wrapper-catalog">
-      <div className="container-busca">
-        <form>
-          <div className="input-group flex-nowrap search-box">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Digite sua busca"
-              aria-label="Digite sua busca"
-              aria-describedby="addon-wrapping"
-            />
-          </div>
-          <button type="button" className="btn btn-primary search-btn">
-            BUSCAR
-          </button>
-        </form>
+      <div className="container">
+        <div className="container-busca">
+          <form>
+            <div className="input-group flex-nowrap search-box">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Digite sua busca"
+                aria-label="Digite sua busca"
+                aria-describedby="addon-wrapping"
+              />
+            </div>
+            <button type="button" className="btn btn-primary search-btn">
+              BUSCAR
+            </button>
+          </form>
+        </div>
       </div>
+
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4">
